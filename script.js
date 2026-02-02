37c062d37acc1be2612a48bcfe6cb2ad1b04f387
Sakshi Gautam <sakshigautam90111@gmail.com>
2026-02-02 23:47:18 +0530

    this is updated code

---
+++ script.js
@@ -0,0 +1,8 @@
+function calculateRent() {
+  let roomtype = document.getElementById("roomtype").value;
+  let roomsize = document.getElementById("roomsize").value;
+  let location = document.getElementById("location").value;
+  let privacy = document.getElementById("privacy").value;
+
+  if()
+}
